3279ae08449e0cf8e0657b4c41149fb303c98889
Eunice Lim <eunicelim31@gmail.com>
2021-02-13 22:41:49 -0600

    week 6 ems

--- ems/app.js
+++ ems/app.js
@@ -14,16 +14,33 @@ app.set("view engine", "ejs");
 
 app.use(logger("short"));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function (request, response) {
-
     response.render("index", {
-
-        title: "Home page"
-
+        message  : "Easy Employee Management System"
     });
-
 });
 
+app.get("/view", function (request, response) {
+    response.render("view", {
+        message  : "View your selected employee here"
+    });
+});
+
+app.get("/list", function (request, response) {
+    response.render("list", {
+        message  : "View of employee records"
+    });
+});
+
+
+
+app.get("/new", function (request, response) {
+    response.render("new", {
+        message  : "Enter new employee information here"
+    });
+});
 http.createServer(app).listen(8080, function() {
 
     console.log("Application started on port 8080!");
